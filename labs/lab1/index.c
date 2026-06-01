d872f6b51d88d2a74f4812c938ed12b2e1a7a966
Viplav Nagpal <viplavnagpal1704@gmail.com>
2026-06-01 04:10:39 -0400

    Minor changes to README.md for Lab 1 and added pdf equivalent from pandoc for submission

---
+++ labs/lab1/index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Content-Type: text/html\n\n");
+
+	printf("<!DOCTYPE html>\n");
+	printf("<html>\n");
+	printf("<head>\n");
+
+	printf("<title>Course - Waph</title>\n");
+
+	printf("</head>\n");
+
+	printf("<body>");
+
+	printf("<h1>My name is Viplav Nagpal</h1>\n");
+	printf("<p>I am a Junior at UC and am taking this course to learn how to make websites and figure out possible vulnerabilities</p>\n");
+
+	printf("</body>\n");
+	printf("</html>\n");
+
+	return 0;
+
+}
